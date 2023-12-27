90fce95f347e30b3e90dcfe62cd430f7dfeca653
kangmoonhye <ansgp0323@naver.com>
2023-12-27 12:12:41 +0900

    [Docs] 우프 프로젝트 백엔드 개발

--- src/main/java/com/example/WoofBackend_Develop/signup/controller/SignupController.java
+++ src/main/java/com/example/WoofBackend_Develop/signup/controller/SignupController.java
@@ -1,11 +1,11 @@
 package com.example.WoofBackend_Develop.signup.controller;
 
-import io.swagger.annotations.Api;
+//import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Api(value = "회원가입 컨트롤러 v1", tags = "회원가입 API")
+//@Api(value = "회원가입 컨트롤러 v1", tags = "회원가입 API")
 @RequestMapping("/signup")
 public class SignupController {
 
